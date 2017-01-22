0f20ee649508dc5176d2dc92ba2e8edf1bbcbf74
thomasfouan <thomasfouan@hotmail.fr>
2017-01-22 18:55:38 +0100

    oublie de add les 3 fichiers ressources dans le commit precedent

---
+++ app/src/main/java/com/esgi/androidproject/GoogleMapPopup.java
@@ -0,0 +1,41 @@
+package com.esgi.androidproject;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by thomasfouan on 22/01/2017.
+ */
+
+public class GoogleMapPopup implements GoogleMap.InfoWindowAdapter {
+
+    View view;
+
+    public GoogleMapPopup(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+
+        TextView name = (TextView) view.findViewById(R.id.restaurantName);
+        TextView note = (TextView) view.findViewById(R.id.restaurantNote);
+
+        if(marker.getTitle() != null && !marker.getTitle().equals("")) {
+            name.setText(marker.getTitle());
+        } else {
+            name.setText("Döner Kebab");
+            note.setText("★★★★☆");
+        }
+
+        return view;
+    }
+}
